7436b555ec16abf35ac64b7089d248d7146fa567
mauriceliddy <maurice.liddy@revature.net>
2021-03-26 23:09:06 +0000

    First commit from the Lenovo

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App{
+
+    public static void main(String[] args) {
+        System.out.println("hello World");
+        doSomething("Maurice");
+    }
+   
+    public static void doSomething(String name) {
+        System.out.println("Doing something now: " + name);
+        
+    }
+   }
---
+++ README.md
@@ -0,0 +1,8 @@
+# App 
+A jank Example from the first day. Basic
+Basic Hello world but also includes a jankgradle example
+
+Includes javadocs (/**)
+
+
+Also has git set up and all that business
